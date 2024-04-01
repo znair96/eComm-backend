fa09dfe299cfefb2da991b3229aca93daf3e8b80
znair96 <zubnair2396@gmail.com>
2024-04-01 13:56:46 +0530

    added config for remote mongo connection

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   DB_NAME: "ecomm_db",
-  DB_URL: "mongodb://localhost/ecomm_db",
+  DB_URL: "mongodb+srv://zubnair1996:soccerchamp@cluster0.tmesugw.mongodb.net/?retryWrites=true&w=majority/ecomm_db",
 };
